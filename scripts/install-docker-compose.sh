3d9b0a51c15b1b82eb358fa1b9d0c2c2ac4bdc2d
Lev Khoroshansky <sololev27@gmail.com>
2022-04-04 15:14:55 +0200

    fix: Executable

--- scripts/install-docker-compose.sh
+++ scripts/install-docker-compose.sh
@@ -1,7 +1,13 @@
 #! /usr/bin/env bash
 
+# Bash "strict" mode.
+set -euxo pipefail
+
 # Install Docker Compose.
 export DOCKER_CONFIG="/usr/local/lib/docker"
 sudo mkdir -p $DOCKER_CONFIG/cli-plugins
 sudo curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+
+# Make it executable.
+sudo chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 
