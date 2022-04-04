9442618ce253f32f02ce1b40195be17279dd2af3
Lev Khoroshansky <sololev27@gmail.com>
2022-04-04 14:56:40 +0200

    refactor: Split Docker scripts

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -11,8 +11,3 @@ sh ./get-docker.sh
 sudo usermod -aG docker $USER
 newgrp docker
 
-# Install Docker Compose.
-DOCKER_CONFIG="/usr/local/lib/docker/"
-sudo mkdir -p $DOCKER_CONFIG/cli-plugins
-sudo curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-
