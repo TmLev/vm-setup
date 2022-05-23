421b7d5aad58f71f7a2d1fa3d7996b29da7ba0b3
Lev Khoroshansky <sololev27@gmail.com>
2022-05-23 20:56:15 +0200

    fix: Make dirs under sudo

--- scripts/add-ssh-keys.sh
+++ scripts/add-ssh-keys.sh
@@ -7,8 +7,8 @@ SSH_USER=$1
 SSH_PUBLIC_KEYS_PATH=$2
 
 if id $SSH_USER >/dev/null 2>&1; then
-	mkdir -p /home/$SSH_USER/.ssh
-	cp $SSH_PUBLIC_KEYS_PATH /home/$SSH_USER/.ssh/authorized_keys
+	sudo mkdir -p /home/$SSH_USER/.ssh
+	sudo cp $SSH_PUBLIC_KEYS_PATH /home/$SSH_USER/.ssh/authorized_keys
 	sudo chown -R $SSH_USER /home/$SSH_USER/.ssh
 	sudo chgrp -R $SSH_USER /home/$SSH_USER/.ssh
 else
